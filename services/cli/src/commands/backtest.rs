0daa8a6835ff311b107224b81ab55972f6dd553b
LHF009 <rodriguezpatricio567@gmail.com>
2025-07-16 15:20:59 -0300

    added env

--- services/cli/src/commands/backtest.rs
+++ services/cli/src/commands/backtest.rs
@@ -1,43 +1,43 @@
-use anyhow::{Ok, Result, anyhow};
+use anyhow::{Ok, Result};
 use clap::Parser;
 
 #[derive(Clone, Debug, Parser)]
 pub struct BacktestArgs {
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     mev_commission_range: u16,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     epoch_credits_range: u16,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     commission_range: u16,
-    #[arg(long, default_value = "1.0")]
+    #[arg(long, env, default_value = "1.0")]
     scoring_delinquency_threshold_ratio: f64,
-    #[arg(long, default_value = "1.0")]
+    #[arg(long, env, default_value = "1.0")]
     instant_unstake_delinquency_threshold_ratio: f64,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     mev_commission_bps_threshold: u16,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     commission_threshold: u8,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     historical_commission_threshold: u8,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     num_delegation_validators: u32,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     scoring_unstake_cap_bps: u32,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     instant_unstake_cap_bps: u32,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     stake_deposit_unstake_cap_bps: u32,
-    #[arg(long, default_value = "1.0")]
+    #[arg(long, env, default_value = "1.0")]
     instant_unstake_epoch_progress: f64,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     compute_score_slot_range: u64,
-    #[arg(long, default_value = "1.0")]
+    #[arg(long, env, default_value = "1.0")]
     instant_unstake_inputs_epoch_progress: f64,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     num_epochs_between_scoring: u64,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     minimum_stake_lamports: u64,
-    #[arg(long, default_value = "1")]
+    #[arg(long, env, default_value = "1")]
     minimum_voting_epochs: u64,
 }
 
